c6f573c288086c6895eae8c0f80b7011ca61bc73
sebastian prieto <js.prieto10@uniandes.edu.co>
2019-06-26 09:38:06 -0500

    cambios extraños que hay en mi

---
+++ server_viz.py
@@ -0,0 +1,23 @@
+    
+from flask import Flask
+from flask import send_file
+import os
+import json
+from flask import Response
+from flask import jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/odsComuna')
+def ods_comuna():
+	with open('comuna_vs_ods.json', 'r') as f:
+		datastore = json.load(f)
+		return jsonify(datastore)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host='0.0.0.0')
