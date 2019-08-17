19c6e8cafea11a6bed3d06dbd95bf7b6dec71f63
sebastian prieto <js.prieto10@uniandes.edu.co>
2019-08-16 19:29:50 -0500

    commit

--- server_viz.py
+++ server_viz.py
@@ -156,4 +156,4 @@ def pa(path):
 
 df = pd.read_pickle('datos_filtrados.pkl')
 if __name__ == "__main__":
-	app.run(debug=True, port=5000, host='0.0.0.0')
+	app.run(debug=True, port=8000, host='0.0.0.0')
